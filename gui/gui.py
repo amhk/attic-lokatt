db268a7b5f6188e8af561d3e7ffe54c265bb442e
Martin Wallgren <martin@wallgren.it>
2015-01-27 14:29:41 +0100

    lokatt.py in root with the UI's as packages
    Move lokatt.py to root directory to keep it as one project with one
    main entry.
    
    Add a base View implementation in the common package to serve as the
    main api for the different UI's.
    
    The packages cli and gui each have their own implementation of View.

---
+++ gui/gui.py
@@ -0,0 +1,20 @@
+from common.view import View
+from common.liblokatt import Message
+
+
+class Gui(View):
+    '''
+    Graphical User Interface implementation of View
+    Not implemented yet!
+    '''
+
+    def __init__(self, logcat):
+        View.__init__(self, logcat)
+
+    def show(self):
+        # Create Window and what ever we need to show a window
+        pass
+
+    def on_message(self, m:Message):
+        pass
+
