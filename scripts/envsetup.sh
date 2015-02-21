a1219932fda5dfd94f91a3b2f201308b7c26a599
Martin Wallgren <martin@wallgren.it>
2015-02-21 20:46:18 +0100

    scripts: envsetup for virtualenv
    envsetup script for setting up a development environment for lokatt.
    
    source scripts/envsetup.sh will set T to the root of the lokatt git and
    add the function lokatt_setup to the shell.
    
    The idea is to run lokatt_setup every time you start working and it will
    create the python virtual environment and keep the dependencies up to
    date.
    
    Signed-off-by: Martin Wallgren <martin@wallgren.it>

---
+++ scripts/envsetup.sh
@@ -0,0 +1,57 @@
+_lokatt_gettop()
+{
+	T=$(git rev-parse --show-toplevel)
+
+	if [[ -z ${T} ]]; then
+		echo "error: failed to get top of lokatt git"
+		return 1
+	fi
+}
+
+# Set T when sourcing this script, abort if we can't find top
+_lokatt_gettop || return 1
+
+_lokatt_activate_venv()
+{
+	if [[ ! -e ${T}/venv/bin/activate ]]; then
+		echo "error: missing virtual environment" >&2
+		echo "hint: run lokatt_setup" >&2
+		return 1
+	fi
+	echo "Activating python environment"
+	source ${T}/venv/bin/activate
+	return $?
+}
+
+_lokatt_create_venv()
+{
+	echo "creating virtual environment..."
+	${PYVENV} ${T}/venv
+	return $?
+}
+
+_lokatt_install_dependencies()
+{
+	echo "Ensuring python dependencies..."
+	pip install -q -r ${T}/scripts/dependencies.txt
+	return $?
+}
+
+lokatt_setup()
+{
+	if [ ! -d "${T}/venv" ]; then
+
+		if [[ -z ${PYVENV} ]]; then
+			PYVENV="$(which pyvenv)"
+		fi
+		if [[ -z "${PYVENV}" ]]; then
+			echo "error: pyvenv not found" >&2
+			echo "hint: set PYVENV" >&2
+			return 2
+		fi
+
+		_lokatt_create_venv || return 3
+	fi
+	_lokatt_activate_venv || return 4
+	_lokatt_install_dependencies || return 5
+}
