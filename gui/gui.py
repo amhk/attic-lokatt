812e8d219b6f8ef7404deb97344af445ccd4c542
Martin Wallgren <martin@wallgren.it>
2015-03-03 21:32:20 +0100

    scripts: autopep8 of all files
    autopep8 format of the project according to the .pep8 config.
    Some long lines were kept in common/lib/run-test.py and
    scripts/pre-commit.
    
    Signed-off-by: Martin Wallgren <martin@wallgren.it>
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- gui/gui.py
+++ gui/gui.py
@@ -3,6 +3,7 @@ from common.liblokatt import Message
 
 
 class Gui(View):
+
     '''
     Graphical User Interface implementation of View
     Not implemented yet!
@@ -15,6 +16,5 @@ class Gui(View):
         # Create Window and what ever we need to show a window
         pass
 
-    def on_message(self, m:Message):
+    def on_message(self, m: Message):
         pass
-
